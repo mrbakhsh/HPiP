d9a9c4a8c91eb9b3dd32a72b1fcb3b76e33f3c5a
Matine <matinerb.94@gmail.com>
2021-09-10 02:06:54 -0600

    chnages data in /inst

---
+++ inst/unitTests/test_calculateAAC.R
@@ -0,0 +1,7 @@
+test_calculateAAC = function() {
+  data(UP000464024_df)
+  x_df <- calculateAAC(UP000464024_df)
+  x_df <- x_df[, -1]
+  checkEqualsNumeric(sum(as.matrix(x_df)), 17, tolerance = 1e-2)
+}
+
