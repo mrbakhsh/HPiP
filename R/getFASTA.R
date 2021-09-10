15b340cef49d2b48a7ba2c35b9abea622a104b98
Matine <matinerb.94@gmail.com>
2021-09-10 13:21:36 -0600

    edit the code
    Former-commit-id: 028df086cd67a703340206406748d18ecf4b072b

--- R/getFASTA.R
+++ R/getFASTA.R
@@ -10,6 +10,7 @@
     #' @return A list containing protein FASTA sequences.
     #' @author Matineh Rahmatbakhsh, \email{matinerb.94@gmail.com}
     #' @importFrom stats na.omit
+    #' @importFrom utils read.delim
     #' @description This function retrieves protein sequences in FASTA format
     #' directly from the UniProt database via UniProt protein IDs. This function
     #' also checks if the amino-acid composition of protein sequences
@@ -21,19 +22,19 @@
     #' uniprot.id <- c("P0DTC4", "P0DTC5", "P0DTC9")
     #' fasta_df <- getFASTA(uniprot.id, filename = 'FASTA.RData', path = local)
     #' head(fasta_df)
-
+    
     getFASTA <- function(uniprot.id, filename = "FASTA.RData",
                          path = "FASTASeq") {
       . <- NULL
       V1 <- NULL
-
+      
       `%nin%` <- Negate(`%in%`)
-
+      
       names <- unique(uniprot.id)
       if (!is.character(names)) names <- as.character(names)
       len <- length(names)
       fastalist.query <- vector("list", len)
-
+      
       for (i in seq_len(len)) {
         fastalist.query[[i]] <-
           tryCatch(suppressWarnings(protr::readFASTA(paste(
@@ -44,9 +45,9 @@
             return(NA)
           })
       }
-
+      
       names(fastalist.query) <- names
-
+      
       # check if protein sequence's amino acid types re in the 20 default types
       s1.check <-
         lapply(fastalist.query, function(x) protr::protcheck(x))
@@ -58,15 +59,15 @@
         .$Uniprot
       fastalist.query <-
         fastalist.query[names(fastalist.query) %nin% s1.check]
-
-
+      
+      
       fastalist.query.out <-
         do.call(rbind, fastalist.query) %>%
         as.data.frame(.) %>%
         rownames_to_column("UniprotKBID") %>%
         rename(FASTASEQ = V1) %>%
         na.omit(.)
-
+      
       if (!file.exists(path)) {
         dir.create(path, recursive = TRUE)
         localPath <- tools::file_path_as_absolute(path)
@@ -76,6 +77,6 @@
         fname <- file.path(path, filename)
         save(fastalist.query.out, file = fname)
       }
-
+      
       return(fastalist.query)
     }
