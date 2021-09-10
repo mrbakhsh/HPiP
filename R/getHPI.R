43cd02c0c136107f6f8334916b8c39fd4f601f44
Matine <matinerb.94@gmail.com>
2021-09-10 02:02:03 -0600

    Fixed conflicts in source codes
    Former-commit-id: 57ccdade97e51de5624ad2d4012e85bfebd96140

--- R/getHPI.R
+++ R/getHPI.R
@@ -1,58 +1,58 @@
+    .getKronecker <- function(A, B) {
+      m <- matrix(0, nrow = nrow(A) * nrow(B), ncol = ncol(A) * ncol(B))
 
-.getKronecker <- function(A, B) {
-    m <- matrix(0, nrow = nrow(A) * nrow(B), ncol = ncol(A) * ncol(B))
-
-    for (i in seq_len(nrow(A))) {
+      for (i in seq_len(nrow(A))) {
         cur_rows <- seq_len(nrow(B)) + (i - 1) * nrow(B)
         for (j in seq_len(ncol(A))) {
-              m[cur_rows, seq_len(ncol(B)) + (j - 1) * ncol(B)] <- A[i, j] * B
-          }
+          m[cur_rows, seq_len(ncol(B)) + (j - 1) * ncol(B)] <- A[i, j] * B
+        }
+      }
+      m
     }
 
-    m
-}
+    #' getHPI
+    #' @title Generating Host-Pathogen Protein-Protein Interaction (HP-PPI)
+    #' Descriptors
+    #' @param pathogenData The pathogen descriptor matrix.
+    #' @param hostData The host descriptor matrix.
+    #' @param type The interaction type, one or two of
+    #' \code{"combine"} and \code{"kron.prod"}.
+    #' @return A matrix containing the Host-Pathogen Protein-Protein Interaction
+    #' (HP-PPI) descriptors.
+    #' @author Matineh Rahmatbakhsh \email{matinerb.94@gmail.com}
+    #' @description This function calculates Host-Pathogen Protein-Protein
+    #' Interaction (HP-PPI) descriptors via two approaches
+    #' \itemize{
+    #' \item \code{combine} - combine the two descriptor matrix,
+    #' result has \code{(p1 + p2)} columns
+    #' \item \code{kron.prod} - if A has m x n matrix and B is q x p matrix,
+    #' then the Kronecker product is the code{(pm × qn)} block matrix
+    #' }
+    #' @export
+    #' @examples
+    #' x <- matrix(c(1, 2, 3, 1), nrow = 2, ncol = 2, byrow = TRUE)
+    #' y <- matrix(c(0, 3, 2, 1), nrow = 2, ncol = 2, byrow = TRUE)
+    #' getHPI(x, y, "combine")
+    #' getHPI(x, y, "kron.prod")
+    getHPI <- function(pathogenData, hostData, type = c("combine", "kron.prod")) {
+      if (!is.matrix(pathogenData)) pathogenData <- as.matrix(pathogenData)
+      if (!is.matrix(hostData)) hostData <- as.matrix(hostData)
 
-#' getHPI
-#' @title Generating Host-Pathogen Protein-Protein Interaction (HP-PPI) Descriptors
-#' @param pathogenData The pathogen descriptor matrix.
-#' @param hostData The host descriptor matrix.
-#' @param type The interaction type, one or two of
-#' \code{"combine"} and \code{"kron.prod"}.
-#' @return A matrix containing the Host-Pathogen Protein-Protein Interaction (HP-PPI) descriptors.
-#' @author Matineh Rahmatbakhsh \email{matinerb.94@gmail.com}
-#' @description This function calculates Host-Pathogen Protein-Protein Interaction (HP-PPI) descriptors
-#' via two approaches
-#' \itemize{
-#' \item \code{combine} - combine the two descriptor matrix,
-#' result has \code{(p1 + p2)} columns
-#' \item \code{kron.prod} - if A has m x n matrix and B is q x p matrix,
-#' then the Kronecker product is the code{(pm × qn)} block matrix
-#' }
-#' @export
-#' @examples
-#' x <- matrix(c(1, 2, 3, 1), nrow = 2, ncol = 2, byrow = TRUE)
-#' y <- matrix(c(0, 3, 2, 1), nrow = 2, ncol = 2, byrow = TRUE)
-#' getHPI(x, y, "combine")
-#' getHPI(x, y, "kron.prod")
-getHPI <- function(pathogenData, hostData, type = c("combine", "kron.prod")) {
-    if (!is.matrix(pathogenData)) pathogenData <- as.matrix(pathogenData)
-    if (!is.matrix(hostData)) hostData <- as.matrix(hostData)
+      pathogenRow <- nrow(pathogenData)
+      hostRow <- nrow(hostData)
 
-    pathogenRow <- nrow(pathogenData)
-    hostRow <- nrow(hostData)
-
-    if (pathogenRow != hostRow) stop("Matrix row count must match")
-    if (missing(type)) stop("Must provide at least one interaction type")
+      if (pathogenRow != hostRow) stop("Matrix row count must match")
+      if (missing(type)) stop("Must provide at least one interaction type")
 
 
-    if (type == "combine") {
+      if (type == "combine") {
         result <- cbind(pathogenData, hostData)
-    } else if (type == "kron.prod") {
+      } else if (type == "kron.prod") {
         result <- .getKronecker(pathogenData, hostData)
-    } else {
+      } else {
         stop("Interaction type must be  kron or combine")
+      }
+
+
+      return(result)
     }
-
-
-    return(result)
-}
