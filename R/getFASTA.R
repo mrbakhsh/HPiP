e91f7b82d5bfd8735208d98a30461faa4bc37b3b
Matine <matinerb.94@gmail.com>
2021-09-11 14:11:05 -0600

    remove supp.war
    Former-commit-id: 6e3598860e2a73284e168528a201bb0c997e7c2a

--- R/getFASTA.R
+++ R/getFASTA.R
@@ -22,32 +22,32 @@
     #' uniprot.id <- c("P0DTC4", "P0DTC5", "P0DTC9")
     #' fasta_df <- getFASTA(uniprot.id, filename = 'FASTA.RData', path = local)
     #' head(fasta_df)
-    
+
     getFASTA <- function(uniprot.id, filename = "FASTA.RData",
                          path = "FASTASeq") {
       . <- NULL
       V1 <- NULL
-      
+
       `%nin%` <- Negate(`%in%`)
-      
+
       names <- unique(uniprot.id)
       if (!is.character(names)) names <- as.character(names)
       len <- length(names)
       fastalist.query <- vector("list", len)
-      
+
       for (i in seq_len(len)) {
         fastalist.query[[i]] <-
-          tryCatch(suppressWarnings(protr::readFASTA(paste(
+          tryCatch(protr::readFASTA(paste(
             "https://www.uniprot.org/uniprot/", names[i], ".fasta",
             sep = ""
-          )))[[1]], error = function(err) {
+          ))[[1]], error = function(err) {
             cat("Error detected --> NA substituted\n")
             return(NA)
           })
       }
-      
+
       names(fastalist.query) <- names
-      
+
       # check if protein sequence's amino acid types re in the 20 default types
       s1.check <-
         lapply(fastalist.query, function(x) protr::protcheck(x))
@@ -59,15 +59,15 @@
         .$Uniprot
       fastalist.query <-
         fastalist.query[names(fastalist.query) %nin% s1.check]
-      
-      
+
+
       fastalist.query.out <-
         do.call(rbind, fastalist.query) %>%
         as.data.frame(.) %>%
         rownames_to_column("UniprotKBID") %>%
         rename(FASTASEQ = V1) %>%
         na.omit(.)
-      
+
       if (!file.exists(path)) {
         dir.create(path, recursive = TRUE)
         localPath <- tools::file_path_as_absolute(path)
@@ -77,6 +77,6 @@
         fname <- file.path(path, filename)
         save(fastalist.query.out, file = fname)
       }
-      
+
       return(fastalist.query)
     }
