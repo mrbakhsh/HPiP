c9ef70b84eeb2533f41165e5068b5cbe0d4f2ae6
Matineh Rahmatbakhsh <m_rb@Matinehs-MBP.ht.home>
2023-03-19 08:44:38 -0600

    fixed the unit test

--- inst/unitTests/test_calculateBE.R
+++
@@ -1,6 +0,0 @@
-test_calculateBE = function() {
-  data('UP000464024_df')
-  x_df <- calculateBE(UP000464024_df)
-  x_df <- x_df[, -1]
-  checkEqualsNumeric(sum(as.matrix(x_df)), 304, tolerance = 1e-2)
-}
